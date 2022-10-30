04312c501e05afb5c4472a254513c492ebfe0ad7
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-10-30 10:03:29 -0400

    cleaning up app.js and app.css; created new folders for reviews

--- AvoReact/src/components/Home.js
+++ AvoReact/src/components/Home.js
@@ -6,12 +6,6 @@ const Home = (props) => {
 
 	return (
 		<>
-			<head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link href="https://fonts.googleapis.com/css2?family=Swanky+and+Moo+Moo&display=swap" rel="stylesheet" />
-			</head>
-
 			<body style={{font:"font-family: 'Swanky and Moo Moo', cursive;"}}>
 				<div className='container-md'>
 					<h2>Welcome to The Good Avocado</h2>
