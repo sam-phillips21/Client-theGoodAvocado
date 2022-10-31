2a3c292af6fc50d504a5afcb934520acb1dea7ba
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-10-31 11:36:08 -0400

    editReview.js updated

---
+++ AvoReact/src/components/Reviews/IndexReview.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react' 
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { reviewIndex } from '../../api/review'
+// import Image from 'react-bootstrap/Image'
+import images from '../shared/images'
+
+const cardContainerLayout = {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'center'
+}
+
+
+
+const ReviewIndex = ({ user, msgAlert }) => {
+
+    const [allReviews, setAllReviews] = useState([])
+
+    useEffect(() => {
+        reviewIndex(user)
+        .then(res => {
+            setAllReviews(res.data.restaurants)
+        })
+        .catch((error) => {
+            msgAlert({
+                heading: 'Failure',
+                message: 'Index Failure' + error,
+                variant: 'danger'
+            })
+        })
+    }, [])
+
+    const restaurantCards = allReviews.map(review => (
+        
+        <Card key={ review.id } style={{ width: '25rem', margin: 5 }}>      
+            {
+                images.map(image => (
+                    <Card.Img variant="top" src={ image } />
+                ))
+                
+            }
+            
+            <Card.Header>{ review.name }</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    <Link to={ `/reviews/${restaurant._id }` }>View { restaurant.name } </Link>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    ))
+
+    return (
+        <div className='container-md' style={ cardContainerLayout }>
+            { restaurantCards }
+        </div>
+    )
+}
+
+export default ReviewIndex
