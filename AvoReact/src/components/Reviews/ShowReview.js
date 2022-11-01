ee128a2239413fc3ed4e0f2e97f4e461f080a208
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-01 11:13:20 -0400

    show review updated

--- AvoReact/src/components/Reviews/ShowReview.js
+++ AvoReact/src/components/Reviews/ShowReview.js
@@ -1,129 +1,70 @@
-import Accordion from 'react-bootstrap/Accordion'
-import React, { useEffect, useState } from 'react' 
-import { useParams, useNavigate } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
-import { reviewUpdate, reviewDelete } from '../../api/review'
-import {EditReview, handleChange} from '../Reviews/EditReview'
+import React, { useState } from 'react' 
+import { Card, Button } from 'react-bootstrap'
+import { reviewDelete } from '../../api/review'
+import EditReview from '../Reviews/EditReview'
+
 // import LoadingScreen from '../LoadingScreen'
-import ReviewForm from '../shared/ReviewForm'
-
-
-// const cardContainerLayout = {
-//     display: 'flex',
-//     flexFlow: 'row wrap',
-//     justifyContent: 'center'
-// }
-
 
 const ShowReview = (props) => {
     const {review, restaurant, user, msgAlert, triggerRefresh, handleChange} = props
-    // const [setReview] = useState(null)
-    // const [isUpdateShown, setIsUpdateShown] = useState(false)
-    // const [editShow, setEditShow] = useState(false)
+    // console.log('this is the props, props')
 
-    // const [NewReview, setNewReview] = useState
-    const [deleted, setDeleted] = useState(false)
-    // const [updated, setUpdated] = useState(false)
+    const [editModalShow, setEditModalShow] = useState(false)
 
-    // const { id } = useParams()
-    // const navigate = useNavigate()
-
-    // useEffect(() => {
-    //     ShowReview(user, id)
-    //         .then(res => {
-          
-    //            ShowReview(res.data.review)
-    //         })
-    //         .catch((error) => {
-    //             msgAlert({
-    //                 heading: 'Failure',
-    //                 message: 'Show Review Failure' + error,
-    //                 variant: 'danger'
-    //             })
-    //         })
-    // }, [updated])
 
     const handleDeleteReview = () => {
         reviewDelete(user, review._id, restaurant._id)
         .then(() => {
-            setDeleted(true)
             msgAlert({
-                heading: 'Success',
-                message: 'Deleting a Review',
+                heading: 'Success: Review Deleted',
+                message: "We'll never speak of it again",
                 variant: 'success'
-            })
-            
+            })  
         })
         .then(() => triggerRefresh())
         .catch((error) => {
             msgAlert({
-                heading: 'Failure',
-                message: 'Deleting  Review Fail: ' + error,
+                heading: 'Oops',
+                message: 'Delete Review Fail: ' + error,
                 variant: 'danger'
             })
         })
     }
 
-    // let reviewAccordion
-    // if (review) {
-    //     if (review.length > 0) {
-    //         // map over the reviews
-    //         // produce one ShowReview component for each of them
-    //         reviewAccordion = review.reviews.map(review => (
-    //             <ShowReview 
-    //                 key={review._id}
-    //                 review={review}
-    //                 user={user}
-    //                 msgAlert={msgAlert}
-    //                 triggerRefresh={() => setUpdated(prev => !prev)}
-    //             />
-    //         ))
-    //     }
-    // }
-
-    // if (deleted) navigate('/restaurants')
-
-    // if (!restaurant) {
-    //     return <LoadingScreen />
-    // }
-
-    // if (!review) {
-    //     return <p> ...Loading </p>
-    // }
-
     return (
-		<Accordion defaultActiveKey={['0']} alwaysOpen>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>{user} said:</Accordion.Header>
-                <Accordion.Body>
-                    <small>{review.comment}</small><br/>
-                        {/* <ReviewForm
-                        review={review}
-                        handleDeleteReview={handleDeleteReview}
-                        msgAlert={msgAlert}
-                        // handleChange={handleChange}
-                        // handleSubmit={handleSubmit}
-                        heading={user}
-                        /> */}
-                    <Button
-                        className='m-2'
-                        variant='warning'
-                        onClick={() => reviewUpdate()}
-                    >
-                        Edit
-                    </Button>
-                    <Button
-                        variant="danger"
-                        handleChange = {handleChange}
-                        onClick={() => handleDeleteReview()}
-                    >
-                        Delete
-                    </Button>
+        <>
+            <Card className="m-2">
+                    <Card.Header>{user.email} said:</Card.Header>
+                    <Card.Body>
+                        <small>{review.comment}</small><br/>
 
-                </Accordion.Body>
+                        <Button
+                            className='m-2'
+                            variant='warning'
+                            onClick={() => setEditModalShow}
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            variant="danger"
+                            handleChange = {handleChange}
+                            onClick={() => handleDeleteReview()}
+                        >
+                            Delete
+                        </Button>
 
-            </Accordion.Item>
-        </Accordion>
+                    </Card.Body>
+            </Card>
+            <EditReview 
+                user={user}
+                restaurant={restaurant}
+                review={review}
+                msgAlert={msgAlert}
+                triggerRefresh={triggerRefresh}
+                show={editModalShow}
+                handleClose={() => setEditModalShow(false)}
+            />
+        </>
     )
 }
 
