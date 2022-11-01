9609e5b2982534197ddbfed6766fe6dbc02fda1a
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-01 16:56:48 -0400

    image updated

--- AvoReact/src/components/Home.js
+++ AvoReact/src/components/Home.js
@@ -8,7 +8,7 @@ const Home = (props) => {
 
 		<div className='container-sm'>
 			<h2>Welcome to The Good Avocado </h2>
-			<img className='rounded-bottom show-image' src="https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="a table full of various meals" />
+			<img className='rounded img-fluid body-bg' src="https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="a table full of various meals" />
 			<RestaurantIndex msgAlert= {msgAlert}/>
 		</div>
 
