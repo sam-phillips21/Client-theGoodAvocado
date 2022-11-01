c840776d72ac9175d86857b07205e81645f377a1
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-01 18:31:59 -0400

    homepage new image

--- AvoReact/src/components/Home.js
+++ AvoReact/src/components/Home.js
@@ -8,7 +8,7 @@ const Home = (props) => {
 		<>
 			<div className='container-sm'>
 				<h2>Welcome to The Good Avocado </h2>
-				<img className='rounded img-fluid body-bg' src="https://images.unsplash.com/photo-1631100732613-6b65da9a343d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="a table full of various meals" />
+				<img className='rounded img-fluid body-bg' src="https://images.pexels.com/photos/511763/pexels-photo-511763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="a table full of various meals" />
 			</div>
 			<div>
 				<br/>
