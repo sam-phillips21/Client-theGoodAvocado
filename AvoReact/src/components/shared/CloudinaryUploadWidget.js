9a5f38d03355b66d11ddaf8de8f1537d30c5ffcd
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-02 18:19:44 -0400

    public id is the key?

--- AvoReact/src/components/shared/CloudinaryUploadWidget.js
+++ AvoReact/src/components/shared/CloudinaryUploadWidget.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Axios from 'axios'
 import { Button } from 'react-bootstrap'
+import { Image } from 'cloudinary-react'
 
 const CloudinaryUploadWidget = () => {
 
@@ -9,7 +10,7 @@ const CloudinaryUploadWidget = () => {
     const uploadImage = (files) => {
         // console.log(files[0])
         const formData = new FormData ()
-        formData.append("file", files[0])
+        formData.append("file", imageSelected)
         formData.append("upload_preset", "gxc7sx3v")
         
         Axios.post("https://api.cloudinary.com/v1_1/dtszeeznm/image/upload", formData).then((response) => {
@@ -17,17 +18,24 @@ const CloudinaryUploadWidget = () => {
         });
     };
 
+    let public_id = null
 
     return (
         <div>
             <input
                 type="file"
-                onChange={(e) => {setImageSelected(e.target.files)}}
+                onChange={(e) => {setImageSelected(e.target.files[0])}}
             />
             <Button id="upload_widget" className="m-2 cloudinary-button btn-secondary" onClick={uploadImage}
             >
                 Upload
             </Button>
+
+            <Image 
+                style={{width: 200}}
+                cloudName="dtszeeznm" 
+                publicId= {`https://res.cloudinary.com/dtszeeznm/image/upload/v1667425812/${public_id}.jpg`}
+            />
         </div>
     );
 
