53a8043faf18a0eab635724f1615c02739464d09
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-03 11:10:15 -0400

    grow instead of spin

--- AvoReact/src/components/LoadingScreen.js
+++ AvoReact/src/components/LoadingScreen.js
@@ -2,7 +2,7 @@ import { Spinner } from 'react-bootstrap'
 
 const LoadingScreen = () => (
     <div style={{textAlign: 'center', height: 2000 }}>
-        <Spinner role='status' animation='border' size='xxl'>
+        <Spinner role='status' animation='grow' size='xxl'>
         <span className='visually-hidden'>Loading...</span>
         </Spinner>
     </div>
